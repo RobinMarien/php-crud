93ba11c84d5b63f9b9e81a1bc82ee5366bb4c110
Manny <manny-apsel@hotmail.com>
2019-10-29 16:33:27 +0100

    made grade class without get/set functions

---
+++ Model/Grade.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+class Grade
+{
+    private $id;
+    private $gradename;
+    private $location;
+
+    public function __construct(int $_id, string $_gradename, string $_location)
+    {
+        $this->id = $_id;
+        $this->gradename = $_gradename;
+        $this->location = $_location;
+    }
+}
